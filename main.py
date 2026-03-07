b8942fad81aa165e0e6db28297bb06a3ad79b5b4
Baljeet <esteban.aguirre@utp.edu.co>
2026-03-06 19:43:43 -0500

    feat: agregando frontend y configuracion limpia

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from google import genai
 from dotenv import load_dotenv
 import os
@@ -6,17 +8,26 @@ import os
 load_dotenv()
 app = FastAPI()
 
+# Servir archivos estáticos desde la carpeta 'static'
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def home():
+    return FileResponse("static/index.html")
+
+
 @app.get("/llm/{prompt}")
 async def read_root(prompt: str):
-
+    # lógica de comunicación con el LLM
     client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-    # CAMBIO: Usamos el ID del modelo más reciente (3.1 Pro)
     response = client.models.generate_content(
-        model="gemini-3.1-pro-preview", 
-        contents=prompt
+        model="gemini-3-flash-preview", contents=prompt
     )
-    
     print(response.text)
-    
+
     return {"Esta es la respuesta": response.text}
+
+
+
